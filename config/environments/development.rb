162e729d55a0c9c840fa5715d16a4875c4562fbd
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-08-08 23:00:37 +0900

    Added user confirmation email -Added a confirmation step, so now when the user registers to the site, will receive a confirmation email and will have no access to login until the user clicks on the verification link. after verifying the email the user will have full access to the site.  Please go the README file to setup the confirmation on local Host. -Added the Mailer Gem that is in charge of sending the confirmation emails via gmail SMTP service. -Updated other files, for example updated the partial login.scss because there was a bug on it that was changing the button size after adding recovery options for the user. -Added Recovery options, Now the user can recover their password via email. just need to click on the recovery links on the login page. the user can even resend a confirmation email after registration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -30,7 +30,17 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
