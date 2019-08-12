0c36bd8a9ef0df206bac203736f6f9666578e00a
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-08-12 10:57:49 +0900

    Seed file Done - Updated the seed file and now it adds 35 users with avatars, posts, and posts with images. it will destroy all users at start but will create all of them without veryfication. - This seed file will send emails for the veryfication code. please ve sure the cloudinary ENV variable is in place or it will fail. - Added images and cover folders to the storage folder on the root of the project. those will ve used for the seeding. - Fixed an error on the navbar that was causing it to go to the center of the page when the user has many posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,74 @@
 require 'faker'
 
-users = User.order(:created_at).take(3)
-50.times do
-  content = Faker::Lorem.sentence
-  users.each { |user| user.miniposts.create!(content: content) }
+# Declaring Variables
+
+cover_array = []
+avatar_array = []
+avatar_counter = 0
+
+puts 'Welcome! This seed file will add some users to the site please hold still!'
+
+puts 'Clearing data....'
+User.destroy_all
+puts "data cleared.."
+puts 'Creating users...'
+
+
+35.times do
+User.create!(
+  email: Faker::Internet.unique.email,
+  username: Faker::Name.unique.middle_name,
+  password: 'secret',
+  bio: Faker::Quote.matz,
+  )
 end
+
+puts "Users created ✅"
+puts "Adding other data to users...."
+
+
+Dir.foreach('./storage/cover_images') do |image|
+  next if image == '.' or image == '..' or image == '.DS_Store'
+  cover_array << image
+end
+
+Dir.foreach('./storage/profile_pics') do |image|
+  next if image == '.' or image == '..' or image == '.DS_Store'
+  avatar_array << image
+end
+
+User.all.each do |user|
+  # Saving user covers randomly
+  user.cover = Pathname.new(Rails.root.join("./storage/cover_images/#{cover_array.sample}")).open
+  user.save
+  # Ordering the Avatar array
+  sorted_avatars = avatar_array.sort_by(&:to_i)
+  # Saving the profile pictures
+  user.avatar = Pathname.new(Rails.root.join("./storage/profile_pics/#{sorted_avatars[avatar_counter]}")).open
+  user.save
+  avatar_counter += 1
+  puts "Images injected #{avatar_counter}"
+end
+
+puts "All user covers and avatars have been added! ✅"
+puts "Adding some posts....."
+
+30.times do
+  user = User.all.sample
+  user.miniposts.create!(
+    content: Faker::Hacker.unique.say_something_smart
+    )
+end
+
+puts "Adding posts with images...."
+
+7.times do
+  user = User.all.sample
+  user.miniposts.create!(
+    content: Faker::GreekPhilosophers.unique.quote,
+    picture: Pathname.new(Rails.root.join("./storage/cover_images/#{cover_array.sample}")).open
+    )
+end
+
+puts "Finished adding data."
+puts "Done! ✅"
